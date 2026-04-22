a41a09e31c64cbccbfb86291c90e158be8c7c6e5
spencerlyon06 <spencerlyon06@gmail.com>
2026-04-22 15:47:18 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,14 @@
+public class Card {
+    String suit;
+    String rank;
+    int value; 
+
+    public Card(String suit, String rank, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+        return rank + " of " + suit;
+    }
+}
---
+++ DeckManager.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DeckManager {
+    ArrayList<Card> cards;
+
+    public DeckManager() {
+        cards = new ArrayList<>();
+        
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        
+        for (String s : suits) {
+            for (int i = 0; i < ranks.length; i++) {
+                cards.add(new Card(s, ranks[i], i + 2));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (cards.size() > 0) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+}
