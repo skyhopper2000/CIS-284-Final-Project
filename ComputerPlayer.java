4aff91332a8ddeb26c900169869c7b2b29a79275
spencerlyon06 <spencerlyon06@gmail.com>
2026-04-30 22:13:58 -0400

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,102 +1,38 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.util.ArrayList;
 
-public class Hand {
-    private ArrayList<Card> cards = new ArrayList<>();
+public class ComputerPlayer extends Player {
 
-    public void addCard(Card c)  { cards.add(c); }
-    public Card get(int index)   { return cards.get(index); }
-    public ArrayList<Card> getCards() { return cards; }
-    public int size()            { return cards.size(); }
-
-    public void replaceCard(int index, Card newCard) {
-        if (index >= 0 && index < cards.size()) {
-            cards.set(index, newCard);
-        }
-    }
-
-    private Map<Integer, Integer> getRankCounts() {
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (Card c : cards) {
-            counts.put(c.getValue(), counts.getOrDefault(c.getValue(), 0) + 1);
-        }
-        return counts;
-    }
-
-    public boolean isPair() {
-        // A pair but NOT two pair or better
-        int pairs = 0;
-        for (int count : getRankCounts().values()) {
-            if (count == 2) pairs++;
-        }
-        return pairs == 1 && !isThreeOfAKind() && !isFourOfAKind();
-    }
-
-    public boolean isTwoPair() {
-        int pairs = 0;
-        for (int count : getRankCounts().values()) {
-            if (count == 2) pairs++;
-        }
-        return pairs == 2;
-    }
-
-    public boolean isThreeOfAKind() {
-        return getRankCounts().containsValue(3) && !getRankCounts().containsValue(2);
-    }
-
-    public boolean isFlush() {
-        String suit = cards.get(0).getSuit();
-        for (Card c : cards) {
-            if (!c.getSuit().equals(suit)) return false;
-        }
-        return true;
-    }
-
-    public boolean isFullHouse() {
-        return getRankCounts().containsValue(3) && getRankCounts().containsValue(2);
-    }
-
-    public boolean isFourOfAKind() {
-        return getRankCounts().containsValue(4);
-    }
-
-    public boolean isStraight() {
-        ArrayList<Integer> values = new ArrayList<>();
-        for (Card c : cards) values.add(c.getValue());
-        Collections.sort(values);
-
-        boolean standard = true;
-        for (int i = 1; i < values.size(); i++) {
-            if (values.get(i) != values.get(i - 1) + 1) { standard = false; break; }
-        }
-        if (standard) return true;
-
-        if (values.contains(14)) {
-            ArrayList<Integer> aceLow = new ArrayList<>(values);
-            aceLow.remove(Integer.valueOf(14));
-            aceLow.add(0, 1);
-            Collections.sort(aceLow);
-            for (int i = 1; i < aceLow.size(); i++) {
-                if (aceLow.get(i) != aceLow.get(i - 1) + 1) return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isStraightFlush() {
-        return isFlush() && isStraight();
-    }
-
-    public boolean isRoyalFlush() {
-        if (!isFlush()) return false;
-        ArrayList<Integer> values = new ArrayList<>();
-        for (Card c : cards) values.add(c.getValue());
-        Collections.sort(values);
-        return values.equals(Arrays.asList(10, 11, 12, 13, 14));
+    ComputerPlayer(int chips, ArrayList<Card> newHand) {
+        super(chips, newHand);
+        this.name = "CPU-" + (int)(Math.random() * 1000);
     }
 
     @Override
-    public String toString() {
-        return cards.toString();
+    protected String makeDecision(int currentBet, BufferedReader mainReader) {
+        Hand h = getHand();
+        if (h.isRoyalFlush() || h.isStraightFlush() || h.isFourOfAKind()
+                || h.isFullHouse() || h.isFlush() || h.isStraight() || h.isThreeOfAKind()) {
+            return "RAISE";
+        } else if (h.isTwoPair() || h.isPair()) {
+            return "CALL";
+        } else {
+            return "FOLD";
+        }
+    }
+
+    @Override
+    protected ArrayList<Card> chooseDiscards(BufferedReader mainReader) {
+        ArrayList<Card> discards = new ArrayList<>();
+        java.util.Map<Integer, Integer> counts = new java.util.HashMap<>();
+        for (Card c : hand.getCards()) {
+            counts.put(c.getValue(), counts.getOrDefault(c.getValue(), 0) + 1);
+        }
+        for (Card c : hand.getCards()) {
+            if (counts.get(c.getValue()) == 1 && discards.size() < 3) {
+                discards.add(c);
+            }
+        }
+        return discards;
     }
 }
