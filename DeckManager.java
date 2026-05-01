dfbb3379621e5426aadd999400adf43ff8b32cf9
spencerlyon06 <spencerlyon06@gmail.com>
2026-04-30 22:12:46 -0400

    Update DeckManager.java

--- DeckManager.java
+++ DeckManager.java
@@ -5,15 +5,9 @@ public class DeckManager {
     ArrayList<Card> cards;
 
     public DeckManager() {
-        /*
-        Class that cotains a deck of Card objects.
-        The deck starts sorted and must be shuffled before use
-        */
         cards = new ArrayList<>();
-        
-        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-        
+        String[] suits  = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks  = {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
         for (String s : suits) {
             for (int i = 0; i < ranks.length; i++) {
                 cards.add(new Card(s, ranks[i], i + 2));
@@ -21,29 +15,16 @@ public class DeckManager {
         }
     }
 
-    public void shuffle() {
-        // randomizes the order of cards in the deck
-        Collections.shuffle(cards);
-    }
+    public void shuffle() { Collections.shuffle(cards); }
 
     public Card drawCard() {
-        // draws a single Card object from the deck
-
-        if (cards.size() > 0) {
-            return cards.remove(0);
-        }
+        if (!cards.isEmpty()) return cards.remove(0);
         return null;
     }
 
-    public ArrayList<Card> drawCards(int num){
-        //draws a given amount of Card objects from the deck
-
-        ArrayList<Card> outList = new ArrayList<>();
-        for(int i=0; i < num; i++){
-            outList.add(drawCard());
-        }
-        return outList;
+    public ArrayList<Card> drawCards(int num) {
+        ArrayList<Card> out = new ArrayList<>();
+        for (int i = 0; i < num; i++) out.add(drawCard());
+        return out;
     }
-
-    
 }
