1466546e27dc05f9b797728907758bd2e37ee485
spencerlyon06 <spencerlyon06@gmail.com>
2026-04-30 21:28:43 -0400

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,15 +1,11 @@
 import java.util.*;
 
-
 public class Hand {
     private ArrayList<Card> cards = new ArrayList<>();
 
     public void addCard(Card c) { cards.add(c); }
-    
     public Card get(int index) { return cards.get(index); }
-    
     public ArrayList<Card> getCards() { return cards; }
-
     public int size() { return cards.size(); }
 
     public void replaceCard(int index, Card newCard) {
@@ -18,42 +14,55 @@ public class Hand {
         }
     }
 
+    private Map<Integer, Integer> getRankCounts() {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (Card c : cards) {
+            counts.put(c.getValue(), counts.getOrDefault(c.getValue(), 0) + 1);
+        }
+        return counts;
+    }
+
     public boolean isPair() {
-        return true;
+        return getRankCounts().containsValue(2);
     }
 
     public boolean isTwoPair() {
-        return false;
+        int pairs = 0;
+        for (int count : getRankCounts().values()) {
+            if (count == 2) pairs++;
+        }
+        return pairs == 2;
     }
 
     public boolean isThreeOfAKind() {
-        return false;
-    }
-
-    public boolean isStraight() {
-        return false;
+        return getRankCounts().containsValue(3);
     }
 
     public boolean isFlush() {
-        return false;
+        String suit = cards.get(0).getSuit();
+        for (Card c : cards) {
+            if (!c.getSuit().equals(suit)) return false;
+        }
+        return true;
     }
 
     public boolean isFullHouse() {
-        return false;
+        return isThreeOfAKind() && isPair();
     }
 
     public boolean isFourOfAKind() {
-        return false;
+        return getRankCounts().containsValue(4);
     }
 
-    public boolean isStraightFlush() {
-        return false;
-    }
-
-    public boolean isRoyalFlush() {
-        return false;
-    }
+    public boolean isStraight() { return false; } 
+    public boolean isStraightFlush() { return false; }
+    public boolean isRoyalFlush() { return false; }
     
+    @Override
+    public String toString() {
+        return cards.toString();
+    }
+}
     @Override
     public String toString() {
         return cards.toString();
