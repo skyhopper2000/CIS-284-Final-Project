4dd2faf8f23beb3e3051571efc499e0e630a7f15
spencerlyon06 <spencerlyon06@gmail.com>
2026-04-30 21:44:01 -0400

    Update pokerUtils.java

--- pokerUtils.java
+++ pokerUtils.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
-
+import java.util.Iterator;
 
 public class pokerUtils {
-    public static int randInt(int min, int max){
-        int randomInt = min + (int)(Math.random() * (max - min + 1));
-        return randomInt;
+
+    public static int randInt(int min, int max) {
+        return min + (int)(Math.random() * (max - min + 1));
     }
- 
+
     public static int evaluateHandType(Hand hand) {
-        // returns the relative precedence of hands
         if (hand.isRoyalFlush()) return 10;
         if (hand.isStraightFlush()) return 9;
         if (hand.isFourOfAKind()) return 8;
@@ -23,72 +21,52 @@ public class pokerUtils {
         return 1;
     }
 
-    public static int evaluateHandRank(Hand hand){
+    public static int evaluateHandRank(Hand hand) {
         HashMap<Integer, Integer> counts = new HashMap<>();
-
-        // count card values
         for (Card c : hand.getCards()) {
             int val = c.getValue();
             counts.put(val, counts.getOrDefault(val, 0) + 1);
         }
 
-        // Find the highest ranking card of the highest value set
-        // I am 99% certain that this will handle all edge cases
         int dominantValue = 0;
         int dominantValueTier = 0;
-        for(int nOfAKind = 1; nOfAKind < 5; nOfAKind++){
-            for (int i = 0; i < hand.getCards().size(); i++) {
-                Card c = hand.getCards().get(i);
-                if (counts.get(c.getValue()) == nOfAKind){
-                    if ((c.getValue() > dominantValue) && (nOfAKind >= dominantValueTier)){
-                        dominantValue = c.getValue();
-                        dominantValueTier = nOfAKind;
-                    }
-                }
+        for (Card c : hand.getCards()) {
+            int count = counts.get(c.getValue());
+            if (c.getValue() > dominantValue && count >= dominantValueTier) {
+                dominantValue = c.getValue();
+                dominantValueTier = count;
             }
         }
-
         return dominantValue;
     }
 
-    public static ArrayList<Player> showdown(ArrayList<Player> players){
-        /*
-        Calculates the winner of the showdown
-        The logic here is omewhat shakey but walk with me here
-        */
+    public static ArrayList<Player> showdown(ArrayList<Player> players) {
+        // FIX: Copy the list so we can safely remove from it without ConcurrentModificationException
+        ArrayList<Player> remaining = new ArrayList<>(players);
 
-        // setup
-        int index = 0;
-        int numTiedPlayers = 0;
-        ArrayList<Player> remainingPlayers = players;
-
-        // Find the player(s) with the most powerful type of hand
-        Player topPlayer = remainingPlayers.get(index);
-        while(numTiedPlayers != remainingPlayers.size()){
-            Player player = remainingPlayers.get(index);
-            if (evaluateHandType(player.getHand()) > evaluateHandType(topPlayer.getHand())){
-                remainingPlayers.remove(topPlayer);
-                topPlayer = player;
-                numTiedPlayers = 0;
-            } else if(evaluateHandType(player.getHand()) < evaluateHandType(topPlayer.getHand())) {
-                remainingPlayers.remove(player);
-            } else {
-                numTiedPlayers++;
-            }
-            index++;
+        // --- Round 1: eliminate by hand type ---
+        int bestType = 0;
+        for (Player p : remaining) {
+            bestType = Math.max(bestType, evaluateHandType(p.getHand()));
         }
-        // if there is a tie, find the high card in the highest group
-        if (remainingPlayers.size() != 1){
-            for (Player player : remainingPlayers){
-                if (evaluateHandRank(player.getHand()) > evaluateHandRank(topPlayer.getHand())){
-                    remainingPlayers.remove(topPlayer);
-                    topPlayer = player;
-                } else if(evaluateHandRank(player.getHand()) < evaluateHandRank(topPlayer.getHand())){
-                    remainingPlayers.remove(player);
-                }
+        // Use iterator to safely remove while iterating
+        Iterator<Player> it = remaining.iterator();
+        while (it.hasNext()) {
+            if (evaluateHandType(it.next().getHand()) < bestType) it.remove();
+        }
+
+        // --- Round 2: break ties by high card in highest group ---
+        if (remaining.size() > 1) {
+            int bestRank = 0;
+            for (Player p : remaining) {
+                bestRank = Math.max(bestRank, evaluateHandRank(p.getHand()));
+            }
+            it = remaining.iterator();
+            while (it.hasNext()) {
+                if (evaluateHandRank(it.next().getHand()) < bestRank) it.remove();
             }
         }
-        return remainingPlayers;
+
+        return remaining;
     }
-
 }
